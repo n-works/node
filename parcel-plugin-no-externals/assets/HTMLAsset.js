13f5b8033cefcb3dde5739c89f0795db707623d6
Tsuyoshi Inamori <inamori@n-works.link>
2020-01-26 13:19:25 +0900

    parcel-plugin-no-externals

---
+++ parcel-plugin-no-externals/assets/HTMLAsset.js
@@ -0,0 +1,31 @@
+const HTMLAsset = require('parcel-bundler/src/assets/HTMLAsset')
+
+module.exports = class HTMLNoExternalsAsset extends HTMLAsset {
+  async collectDependencies () {
+    this.ast = new Proxy(this.ast, {
+      get (target, key) {
+        if (key === 'walk') {
+          return _walk.call(target, target[key])
+        }
+
+        return target[key]
+      }
+    })
+
+    return HTMLAsset.prototype.collectDependencies.call(this)
+  }
+}
+
+function _walk (walk) {
+  return walker => walk.call(this, node => {
+    if (node.attrs && (
+      (node.tag === 'link' && node.attrs.href) ||
+      (node.tag === 'script' && node.attrs.src) ||
+      (node.tag === 'img' && node.attrs.src)
+    )) {
+      return node
+    }
+
+    return walker(node)
+  })
+}
