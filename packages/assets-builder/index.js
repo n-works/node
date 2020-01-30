fc1c59ba9cfc25bdc09eacf3f8894fe6817362db
Tsuyoshi Inamori <inamori@n-works.link>
2020-01-30 14:27:07 +0900

    dotenvの取得をCLIに含める

--- packages/assets-builder/index.js
+++ packages/assets-builder/index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
+const path = require('path')
 const commander = require('commander')
+const dotenv = require('dotenv')
 const ParcelBuilder = require('./builders/ParcelBuilder')
 const ImageminBuilder = require('./builders/ImageminBuilder')
 
 commander
   .option('-w, --watch', 'watch file changes')
   .parse(process.argv)
+
+dotenv.config({
+  path: path.resolve(commander.args[0] || '.env')
+})
 
 const parcelBuilder = new ParcelBuilder(process.env)
 const imageminBuilder = new ImageminBuilder(process.env)
