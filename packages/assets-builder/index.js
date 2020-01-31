13d3dea364054a3f40e52092f7ce68d973230fc5
Tsuyoshi Inamori <inamori@n-works.link>
2020-01-31 09:50:29 +0900

    assets-builder v1.0.5
    * コマンド名を assets-builder に変更
    * 環境ファイル名を .env.assets に変更

--- packages/assets-builder/index.js
+++ packages/assets-builder/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
 const path = require('path')
 const commander = require('commander')
 const dotenv = require('dotenv')
@@ -7,12 +8,14 @@ const ParcelBuilder = require('./builders/ParcelBuilder')
 const ImageminBuilder = require('./builders/ImageminBuilder')
 
 commander
+  .arguments('[envfile]')
   .option('-w, --watch', 'watch file changes')
   .parse(process.argv)
 
-dotenv.config({
-  path: path.resolve(commander.args[0] || '.env')
-})
+const envfilePath = path.resolve(commander.args[0] || '.env.assets')
+if (fs.existsSync(envfilePath)) {
+  dotenv.config({ path: envfilePath })
+}
 
 const parcelBuilder = new ParcelBuilder(process.env)
 const imageminBuilder = new ImageminBuilder(process.env)
